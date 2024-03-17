6ca3fd890333c920791b5baa773db04e73bd0e2c
NuriaMVC <77099034+NuriaMVC@users.noreply.github.com>
2024-03-17 10:49:22 +0100

    Inject the forecast HTML from Javascript

--- src/index.js
+++ src/index.js
@@ -45,9 +45,37 @@ function handleSearchSubmit(event) {
     searchCity(searchInput.value);
 }
 
+function displayForecast(){
+
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function(day){
+        forecastHtml = 
+        forecastHtml + 
+        `
+        <div class = "row">
+        <div class = "col-2">
+                    <div class = "weather-forecast-date">${day}</div>
+                    <img src = "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/shower-rain-night.png" 
+                    alt = ""
+                    width = "36"
+                    />
+                    <div class = "weather-forecast-temperatures">
+                        <span class = "weather-forecast-temperatures-max">18°</span>
+                        <span class = "weather-forecast-temperatures-min">12°</span>
+                    </div>
+                </div>
+        </div>
+                `;
+    }); 
+
+    let forecastElement = document.querySelector ("#forecast");
+    forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Barcelona");
-
-
+displayForecast();
